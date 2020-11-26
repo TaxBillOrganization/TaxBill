e1c06f2174e631a6b32fb549493096c3c1804a39
ardavrl <ardavurl@gmail.com>
2020-11-26 19:51:45 +0300

    First Commit

---
+++ components/Firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Rename this file to "firebaseConfig.js" before use
+// Replace all XXXXs with real Firebase API keys
+
+export default {
+  apiKey: 'AIzaSyASGsRMR7S189ybyTAg5beKTCuqcIUmm20',
+  authDomain: 'taxbill-e4abb.firebaseapp.com',
+  databaseURL: 'https://taxbill-e4abb.firebaseio.com',
+  projectId: 'taxbill-e4abb',
+  storageBucket: 'taxbill-e4abb.appspot.com',
+  messagingSenderId: 'XXXX',
+  appId: '362126127201'
+};
