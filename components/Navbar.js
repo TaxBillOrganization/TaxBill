903148683f55d9f7da36a22d3bef741a67618621
ardavrl <ardavurl@gmail.com>
2020-11-29 17:46:00 +0300

    Update(Navbar)

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,78 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import {Text, View, StyleSheet, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../screens/HomeScreen'
-import SettingsScreen from '../screens/LoginScreen'
+
+import useStatusBar from '../hooks/useStatusBar';
+import { logout } from '../components/Firebase/firebase';
 
 const Tab = createBottomTabNavigator();
 
-
-function Navbar() {
+export default function HomeScreen() {
+  useStatusBar('dark-content');
+  async function handleSignOut() {
+    try {
+      await logout();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  function x() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+         <Button title="Sign Out" onPress={handleSignOut} />
+      </View>
+    );
+  }
+  
+  function y() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>fart!</Text>
+      </View>
+    );
+  }
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name === 'Home') {
+              return (
+                <Ionicons
+                  name={
+                    focused
+                      ? 'ios-information-circle'
+                      : 'ios-information-circle-outline'
+                  }
+                  size={size}
+                  color={color}
+                />
+              );
+            } else if (route.name === 'Settings') {
+              return (
+                <Ionicons
+                  name={focused ? 'ios-list-box' : 'ios-list'}
+                  size={size}
+                  color={color}
+                />
+              );
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={x} options={{ tabBarBadge: 3 }} />
+        <Tab.Screen name="Settings" component={y} />
+      </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import {Text, View, StyleSheet, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import useStatusBar from '../hooks/useStatusBar';
 import { logout } from '../components/Firebase/firebase';
+
+const Tab = createBottomTabNavigator();
 
 export default function HomeScreen() {
   useStatusBar('dark-content');
@@ -13,10 +18,56 @@ export default function HomeScreen() {
       console.log(error);
     }
   }
+  function x() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+         <Button title="Sign Out" onPress={handleSignOut} />
+      </View>
+    );
+  }
+  
+  function y() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Button title="Sign Out" onPress={handleSignOut} />
-    </View>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name === 'Home') {
+              return (
+                <Ionicons
+                  name={
+                    focused
+                      ? 'ios-information-circle'
+                      : 'ios-information-circle-outline'
+                  }
+                  size={size}
+                  color={color}
+                />
+              );
+            } else if (route.name === 'Settings') {
+              return (
+                <Ionicons
+                  name={focused ? 'ios-list-box' : 'ios-list'}
+                  size={size}
+                  color={color}
+                />
+              );
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={x} options={{ tabBarBadge: 3 }} />
+        <Tab.Screen name="Settings" component={y} />
+      </Tab.Navigator>
   );
 }
 
