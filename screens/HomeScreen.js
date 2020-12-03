4ed5749c14be2879f82fd3761e53ffac0244174c
ardavrl <ardavurl@gmail.com>
2020-12-03 22:19:39 +0300

    update(mapSearch)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,48 +1,29 @@
 import React from 'react';
-import {Button, Platform, StyleSheet, Text, View } from 'react-native';
+import {Image,StyleSheet,Text,View,TextInput,TouchableOpacity,KeyboardAvoidingView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-
+import MapView from 'react-native-maps';
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import useStatusBar from '../hooks/useStatusBar';
 import { logout } from '../components/Firebase/firebase';
+import Constants from 'expo-constants';
+import MapContainer from '../containers/MapContainer';
 
-export default
-class App extends React.Component {
-
-    render() {
-      return (
-        
-        <MapView
-            style={{ flex: 1 }}
-            provider={PROVIDER_GOOGLE}
-            showsUserLocation
-            initialRegion={{
-            latitude: 40.742706,
-            longitude: 30.331493,
-            latitudeDelta: 0.0423,
-            longitudeDelta: 0.0321}}
-          ></MapView>
-      ); 
-    } 
-    
-  }
-
-
-/*
-export default function Home(){
-  async function handleSignOut() {
-    try {
-      await logout();
-    } catch (error) {
-      console.log(error);
-    }}
-    return(
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Sign Out" onPress={handleSignOut} />
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapContainer />
       </View>
     );
+  }
 }
-*/
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight
+  }
+});
